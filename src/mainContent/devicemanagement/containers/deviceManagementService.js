14a58e33aaeca03c582cc401447d4d9b73da9078
Himanshu Kumar <Hkumar@sentient-energy.com>
2018-08-10 12:53:06 +0530

    device-management changes

--- src/mainContent/devicemanagement/containers/deviceManagementService.js
+++ src/mainContent/devicemanagement/containers/deviceManagementService.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { BootstrapTable, TableHeaderColumn, SizePerPageDropDown} from 'react-bootstrap-table';
+import { BootstrapTable, TableHeaderColumn, SizePerPageDropDown } from 'react-bootstrap-table';
 import '../../node_modules/bootstrap-table/dist/bootstrap-table.min.css';
 
 
 let order = 'desc';
-class Datatable extends React.Component{
-  constructor(){
-    super(); 
+class Datatable extends React.Component {
+  constructor() {
+    super();
 
-//Checkbox in data table
-  const onSelectAll = (isSelected) => {
+    //Checkbox in data table
+    const onSelectAll = (isSelected) => {
       if (isSelected) {
-         return this.state.dataObject.map(row => row.id);
-       } else {
-         return [];
-       }
+        return this.state.dataObject.map(row => row.id);
+      } else {
+        return [];
+      }
     }
-  // shorting by asc or desc
+    // shorting by asc or desc
     const handleBtnClick = () => {
       if (order === 'desc') {
         this.refs.table.handleSort('asc', 'name');
@@ -30,9 +30,7 @@ class Datatable extends React.Component{
     this.dataObject = [
     ]
   }
-
-  render(){
-
+  render() {
     const selectRowProp = {
       mode: 'checkbox',
       onSelectAll: this.onSelectAll
@@ -60,23 +58,22 @@ class Datatable extends React.Component{
         text: 'All', value: this.dataObject.length
       }]
     };
-         return ( 
-          <div>
-          <BootstrapTable data={ this.dataObject }striped 
-          options={ this.options }
-          selectRow={ selectRowProp }
+    return (
+      <div>
+        <BootstrapTable data={this.dataObject} striped
+          options={this.options}
+          selectRow={selectRowProp}
           pagination
-           hover
-           >
-           <TableHeaderColumn dataField='Id' dataSort={ true } isKey={ true }>Id</TableHeaderColumn>
-          <TableHeaderColumn dataField='SerialNumber' dataSort={ true }>SerialNumber</TableHeaderColumn>
-          <TableHeaderColumn dataField='Phase' dataSort={ true }>Phase</TableHeaderColumn>
-          <TableHeaderColumn dataField='DeviceStatus' dataSort={ true }>DeviceStatus</TableHeaderColumn>
-          <TableHeaderColumn dataField='DeviceState' dataSort={ true }>DeviceState</TableHeaderColumn>
-          <TableHeaderColumn dataField='fwVersion' dataSort={ true }>fwVersion</TableHeaderColumn>
-          <TableHeaderColumn dataField='CommunicationType' dataSort={ true }>CommunicationType</TableHeaderColumn>
-          <TableHeaderColumn dataField='NetworkGroup' dataSort={ true }>NetworkGroup</TableHeaderColumn>
-          </BootstrapTable>  </div>
+          hover >
+          <TableHeaderColumn dataField='Id' dataSort={true} isKey={true}>Id</TableHeaderColumn>
+          <TableHeaderColumn dataField='SerialNumber' dataSort={true}>SerialNumber</TableHeaderColumn>
+          <TableHeaderColumn dataField='Phase' dataSort={true}>Phase</TableHeaderColumn>
+          <TableHeaderColumn dataField='DeviceStatus' dataSort={true}>DeviceStatus</TableHeaderColumn>
+          <TableHeaderColumn dataField='DeviceState' dataSort={true}>DeviceState</TableHeaderColumn>
+          <TableHeaderColumn dataField='fwVersion' dataSort={true}>fwVersion</TableHeaderColumn>
+          <TableHeaderColumn dataField='CommunicationType' dataSort={true}>CommunicationType</TableHeaderColumn>
+          <TableHeaderColumn dataField='NetworkGroup' dataSort={true}>NetworkGroup</TableHeaderColumn>
+        </BootstrapTable></div>
     );
   }
 }
