31404ac27b61d10f3b7b54758b5605643d4458e9
Siva <siva@Macs-MacBook-Pro-2.local>
2018-10-16 16:00:41 +0530

    Sub Header: sub header styles and cleanup

--- src/header/navigation/dashboard/dashboardNavigations.js
+++ src/header/navigation/dashboard/dashboardNavigations.js
@@ -7,15 +7,11 @@ class DashboardNavigations extends Component {
 
     render() {
         return (
-            <Navbar className="dashboard-subnav sub-navigation">
-                <div className="row">
-                    <div className="col-xs-12 col-md-12 sub-menu-wrapper padding-0">
-                        <Nav className="dashboard-subnav">
-                            <DashboardNavigationItem link="/dashboard/home" exact="true">Home</DashboardNavigationItem>
-                            <DashboardNavigationItem link="/dashboard/map-view">Map View</DashboardNavigationItem>
-                        </Nav>
-                    </div>
-                </div>
+            <Navbar fluid fixedTop className="secondary-navbar">
+                <Nav>
+                    <DashboardNavigationItem link="/dashboard/home" exact="true">Home</DashboardNavigationItem>
+                    <DashboardNavigationItem link="/dashboard/map-view">Map View</DashboardNavigationItem>
+                </Nav>
             </Navbar>
         )
     }
